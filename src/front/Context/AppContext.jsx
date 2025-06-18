cb65c45da347ccd230ecef85f51c3acd0b4c4580
Samuel Diaz <90120898+samuelgiorno@users.noreply.github.com>
2025-06-18 17:53:32 +0000

    carpeta context del front

---
+++ src/front/Context/AppContext.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect, createContext } from "react";
+import getState from "flux";
+
+export const Context = createContext(null);
+const AppContext = (PassedComponent) => {
+    const StoreWrapper = (props) => {
+        const [state, setState] = useState(
+            getState({
+                getStore: () => state.store,
+                getActions: () => state.actions,
+                setStore: (updatedStore) =>
+                    setState({ store: Object.assign(state.store, updatedStore), actions: { ...state.actions } })
+            })
+        );
+
+        useEffect(() => {
+            state.actions.syncTokenFromStorage();
+            if (state.store.token) {
+                state.actions.getFavorites();
+            }
+        }, []);
+
+        return (
+            <Context.Provider value={state}>
+                <PassedComponent {...props} />
+            </Context.Provider>
+        );
+    };
+
+    return StoreWrapper;
+};
+
+export default AppContext;
