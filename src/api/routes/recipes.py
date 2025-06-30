fc61ae982af89aac60dcc54477ffa747addbce4e
David S. Mora Toro <39775861+dsmora@users.noreply.github.com>
2025-06-30 19:30:20 +0000

    fixes backend

--- src/api/routes/recipes.py
+++ src/api/routes/recipes.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
-from api.models import Recipe, User
-from ..db import db
+from api.models import db, Recipe, User
 
 api_recipes = Blueprint("api_recipes", __name__)
 
